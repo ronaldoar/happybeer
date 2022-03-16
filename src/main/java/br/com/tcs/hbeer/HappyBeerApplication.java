afdf81eb99198a419389c7743ad4b9b109d8df3b
= <ronaldo.alves.rios@gmail.com>
2022-03-15 22:27:47 -0300

    Incluindo os artefatos iniciais do projeto

---
+++ src/main/java/br/com/tcs/hbeer/HappyBeerApplication.java
@@ -0,0 +1,13 @@
+package br.com.tcs.hbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyBeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/c/hbeer/HappyBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.c.hbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyBeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
